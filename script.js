1cf293734427b54152d435ce49cbbbe1c0c121ef
Alek Nowak <aleknowak@protonmail.com>
2017-06-30 17:56:14 +0000

    Group Project

---
+++ script.js
@@ -0,0 +1,9 @@
+<!-- required snowstorm JS, default behaviour -->
+<script src="snowstorm.js"></script>
+
+<!-- now, we'll customize the snowStorm object -->
+<script>
+snowStorm.snowColor = '#99ccff';   // blue-ish snow!?
+snowStorm.flakesMaxActive = 96;    // show more snow on screen at once
+snowStorm.useTwinkleEffect = true; // let the snow flicker in and out of view
+</script>
